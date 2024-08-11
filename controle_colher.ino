c903a85398057db777ad2eccb95ef12372e49de4
Joao Pedro de Andrade Argenton <153826890+JArgenton@users.noreply.github.com>
2024-08-11 15:17:15 -0300

    Add files via upload

---
+++ controle_colher.ino
@@ -0,0 +1,14 @@
+#include "Wire.h"
+#include "MPU6050"
+
+MPU6050 mpu;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
